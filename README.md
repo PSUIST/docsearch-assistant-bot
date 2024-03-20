9ba62d0825f9b49970f9f1edc774f11d0c61dfdf
Danilo Diaz <ddiaz@live.com>
2024-03-20 09:28:32 -0400

    init

---
+++ README.md
@@ -0,0 +1,50 @@
+## create_assistant.ipynb
+
+This Jupyter notebook is used to create an assistant using Azure OpenAI's API. It cleates a new assistant each time is it ran. It prints the created asistant id which you will use in the test_assistant notebook
+
+It might be a good idea to creat a third notebook that modifies an assistant given an ID.
+
+## test_assistant.ipynb
+
+This is a test harness for the assistant. Make sure you have the right assitant id in the second cell of the notebook.
+
+## FastAPI.py
+
+This creates REST api for the given assistant. The assistant id is configured on line 18. It uses the "Fast" framework.
+
+It creates two api \thread and \message. Thread must be called first to innitiate a thread (sesions). Message allows you to send a question to the assistant and returns the answers from the assistant.
+
+Sample paid loads are available in the test_api.http file.
+
+To run locally type `# uvicorn FastAPI:app --reload` in a command prompt.
+
+## test_api
+
+It uses the VS Code extension "REST Client". Once the extention is install you will see a `Send Request` link on top of each "verb"
+
+## account.py, cmdb.py, crs.py, logs.py
+
+These fuctions provides the assitant with all the data it needs to answer questions. If changes are made to the "signature" of these functions, the appropiate changes need to be made to the assistant to it can understand how to use / call them
+
+## running with copilot studio
+
+if you are running the REST APIs locally, copilot studio can't reach them. To get around this, I used "serveo.net". Serveo is an SSH server just for remote port forwarding. When a user connects to Serveo, they get a public URL that anybody can use to connect to their localhost server.
+
+I open the port transfering by running `ssh -R 80:127.0.0.1:8000 serveo.net` in a command prompt. Make sure to change the ip or host accordingly.
+
+ssh -R 80:127.0.0.1:8000 serveo.net
+
+## sample questions
+
+- What is the application, jvm, host and Port of the affected system whose Port_Name is INDY46 ?
+
+- For Applications whose Port_Name is INDY46, is the host a sox system?
+- For Applications whose Port_Name is INDY46, what are the logs of the affected systems?
+
+- Are there any CRS for the affected system whose application Port_Name is INDY46 ?
+
+## links
+
+- [Getting started with Azure OpenAI Assistants](https://learn.microsoft.com/en-us/azure/ai-services/openai/how-to/assistant)
+
+- [Assistant API docs](https://platform.openai.com/docs/api-reference/assistants)
---
+++ rest_api.py
@@ -0,0 +1,138 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security.api_key import APIKeyHeader, APIKey
+from openai import AzureOpenAI
+import os
+import time
+import json
+from pydantic import BaseModel
+import uvicorn
+from account import get_account
+from cmdb import get_data_for_computer
+from logs import get_logs
+from crs import get_crs
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+#assistant gets created in the notebook. get the id from the notebook and added here
+assistant_id = "asst_czlwrU133DnZcFXNLY2s1iTE"
+    
+client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+    api_version="2024-02-15-preview",
+    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+    )
+
+# Load API key from environment variable. This is used to authenticate requests to the API
+API_KEY = os.getenv("FAST_API_KEY")
+
+# Check for API key
+if API_KEY is None:
+    raise ValueError("API_KEY environment variable not set")
+
+API_KEY_NAME = "access_token"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
+
+async def get_api_key(api_key_header: str = Depends(api_key_header)):
+    if api_key_header == API_KEY:
+        return api_key_header
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+        )
+
+app = FastAPI()
+
+# Pydantic models
+class MessageRequest(BaseModel):
+    message: str
+    thread_id: str
+
+class MessageResponse(BaseModel):
+    message: str
+
+class ThreadResponse(BaseModel):
+    thread_id: str
+
+
+#add the available functions here   
+available_functions = {"get_account": get_account, "get_data_for_computer":get_data_for_computer, "get_logs":get_logs, "get_crs":get_crs}
+
+def wait_for_run(run, thread_id):
+
+    max_steps = 100
+    sleep = 0.5
+    
+    try:
+        cnt = 0
+        while cnt < max_steps:
+            run = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run.id)        
+            cnt += 1
+            if run.status == "requires_action":
+                tool_responses = []
+                if (run.required_action.type == "submit_tool_outputs" and run.required_action.submit_tool_outputs.tool_calls is not None):
+                    tool_calls = run.required_action.submit_tool_outputs.tool_calls
+
+                    for call in tool_calls:
+                        if call.type == "function":
+                            if call.function.name not in available_functions:
+                                raise Exception("Function requested by the model does not exist")
+                            function_to_call = available_functions[call.function.name]                            
+                            tool_response = function_to_call(**json.loads(call.function.arguments))                           
+                            tool_responses.append({"tool_call_id": call.id, "output": tool_response})
+                            print(call.function.name)
+                            print(call.function.arguments)
+                    print(tool_responses)
+                    run = client.beta.threads.runs.submit_tool_outputs(thread_id=thread_id, run_id=run.id, tool_outputs=tool_responses
+                )
+            if run.status == "failed":
+                print("Run failed.")
+                break
+            if run.status == "completed":
+                break
+            time.sleep(sleep)
+
+    except Exception as e:
+        print(e)
+
+    return run
+
+# Define the bot message entry endpoint 
+@app.post("/message/", response_model=MessageResponse)
+async def message(item: MessageRequest, api_key: APIKey = Depends(get_api_key)):
+    print(f"Message received: {item.message}")
+
+    # Send message to assistant
+    message = client.beta.threads.messages.create(
+        thread_id=item.thread_id,
+        role="user",
+        content=item.message
+    )
+
+    run = client.beta.threads.runs.create(
+        thread_id=item.thread_id,
+        assistant_id=assistant_id # use the assistant id defined aboe
+    )
+
+    run = wait_for_run(run, item.thread_id)
+
+    if run.status == 'completed':
+        messages = client.beta.threads.messages.list(limit=1, thread_id=item.thread_id)
+        messages_json = json.loads(messages.model_dump_json())
+        message_content = messages_json['data'][0]['content']
+        text = message_content[0].get('text', {}).get('value')
+        return MessageResponse(message=text)
+    else:
+        return MessageResponse(message="Assistant reported an error.")
+
+
+@app.post("/thread/", response_model=ThreadResponse)
+async def thread(api_key: APIKey = Depends(get_api_key)):
+    thread = client.beta.threads.create()
+    print(f"Thread created with ID: {thread.id}")
+    return ThreadResponse(thread_id=thread.id)
+
+# Uvicorn startup
+# uvicorn FastAPI:app --reload
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8324)
